98b4d660f834a54041ca1b84f06e0f7bce4d9a57
Sahil-tried-coding <sahiltriedcoding@gmail.com>
2024-09-04 13:19:46 -0400

    f

--- redux_op/src/App.jsx
+++ redux_op/src/App.jsx
@@ -4,13 +4,15 @@ import {useDispatch,useSelector} from 'react-redux'
 import { decrement, incByVal, increment } from './redux/CounterSlice';
 
 function App() {
-                  "⬇️-for reding only",      "⬇️-name of state"
-  const counter = useSelector((state)=>state.counterx .value)
+                  "⬇️-for reding only",  "⬇️-store/state",    "⬇️-name of state"
+  const counter = useSelector(          (store)        =>store.counterx .value)
                     "⬇️-dispatch the actions into store/etc"
   const dispatch = useDispatch();
   return (
     <div>
 {counter}
+<input type='text' value={(e)=>incByVal(e.target.value)}/>
+<button>submit</button>
                   {/* "⬇️-dispatch the action name here to reducer"          "⬇️-name of action" */}
 <button onClick={()=>dispatch                                               (increment())}>inc</button>
 <button onClick={()=>dispatch                                               (decrement())}>dec</button>
